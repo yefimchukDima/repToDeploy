b928a084142720b00a35161523bf207fe7cd823f
João Emanuel <joao.emanuel@gmail.com>
2022-10-04 11:06:10 -0300

    fix: add eslint airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,17 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
+  extends: 'airbnb-typescript/base',
+  plugins: ['import', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variable',
+        format: ['camelCase'],
+      },
+    ],
   },
 };

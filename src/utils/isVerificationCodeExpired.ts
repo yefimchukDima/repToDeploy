71064a1a261dae6ab0a3d57f8a948d3eaaad3eb4
João Emanuel <joao.emanuel@gmail.com>
2022-10-06 17:09:01 -0300

    fix: add millis constant

--- src/utils/isVerificationCodeExpired.ts
+++ src/utils/isVerificationCodeExpired.ts
@@ -1,9 +1,11 @@
 import VerificationCodeEntity from 'src/entities/verification_code.entity';
 
+const MILLISECONDS_TO_SECONDS = 1000;
+
 export default function isVerificationCodeExpired(
   verificationCode: VerificationCodeEntity,
 ) {
   return (
-    +verificationCode.expTime - Math.floor(new Date().getTime() / 1000) <= 0
+    +verificationCode.expTime - Math.floor(new Date().getTime() / MILLISECONDS_TO_SECONDS) <= 0
   );
 }

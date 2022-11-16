8676a7d3541d67f1165e5abfa7a457d9b4bdd234
João Emanuel <joao.emanuel@gmail.com>
2022-11-16 15:29:16 -0300

    fix: file uploading

--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -17,22 +17,48 @@ export default class FilesService {
   async uploadToS3(files: Express.Multer.File[]): Promise<FilesDTO[]> {
     const returningFiles: { name: string; location: string }[] = [];
 
-    for (const { originalname, buffer } of files) {
-      try {
-        const { Location } = await this.S3Instance.upload({
-          Bucket: this.configService.get('S3_FILES_BUCKET_NAME'),
-          Key: originalname,
-          Body: buffer,
-        }).promise();
+    const upload = async (originalname: string, buffer: Buffer) => {
+      const { Location } = await this.S3Instance.upload({
+        Bucket: this.configService.get('S3_FILES_BUCKET_NAME'),
+        Key: originalname,
+        Body: buffer,
+      }).promise();
 
-        returningFiles.push({
-          name: originalname,
-          location: Location,
-        });
+      return Location;
+    };
+
+    if (!Array.isArray(files)) {
+      try {
+        const location = await upload(
+          (files as Express.Multer.File).originalname,
+          (files as Express.Multer.File).buffer,
+        );
+
+        return [
+          {
+            location,
+            name: (files as Express.Multer.File).originalname,
+          },
+        ];
       } catch (error) {
         throw new InternalServerErrorException(
           'There was an error on file uploading: ' + error,
         );
+      }
+    } else {
+      for (const { originalname, buffer } of files) {
+        try {
+          const location = await upload(originalname, buffer);
+
+          returningFiles.push({
+            name: originalname,
+            location,
+          });
+        } catch (error) {
+          throw new InternalServerErrorException(
+            'There was an error on file uploading: ' + error,
+          );
+        }
       }
     }
 

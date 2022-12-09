ffb4c5ac8f83ccd556d758a6fa51fa98c085dbe5
yefimchukDima <dmitriy.yefimchuk@mail.zenbit.tech>
2022-12-09 15:56:54 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //test
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: '*',
